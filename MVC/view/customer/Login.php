085ff1237d11758548f50233b3fe85b321e76c21
bigzenoh <bigzenoh@users.noreply.github.com>
2020-12-16 17:30:42 +0700

    hmm

---
+++ MVC/view/customer/Login.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>
+        </title>
+        <meta charset="utf-8">
+            <link href="../../public/css/style.css" rel="stylesheet" type="text/css">
+                <link href="../../public/fontawesome/css/all.css" rel="stylesheet" type="text/css">
+                    <meta content="width=device-width, initial-scale=1" name="viewport">
+                    </meta>
+                </link>
+            </link>
+        </meta>
+    </head>
+    <body>
+        <div class="container">
+            <div class="login-form">
+                <form action="" method="post">
+                    <h1>
+                        Đăng nhập vào website
+                    </h1>
+                    <div class="input-box">
+                        <i>
+                        </i>
+                        <input placeholder="Nhập username" type="text">
+                        </input>
+                    </div>
+                    <div class="input-box">
+                        <i>
+                        </i>
+                        <input placeholder="Nhập mật khẩu" type="password">
+                        </input>
+                    </div>
+                    <div class="btn-box">
+                        <button type="submit">
+                            Đăng nhập
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </body>
+</html>
