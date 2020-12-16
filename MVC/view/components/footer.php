1755deba263d86ceab0a2cd765b09de1b4fa863b
Do Trong Hieu <hieu.dt173116@sis.hust.edu.vn>
2020-12-16 21:16:13 +0700

    da

--- MVC/view/components/footer.php
+++
@@ -1,9 +0,0 @@
-<hr width="80%" align="center" size="1%">
-<div class="footer">
-	<div>
-		<h2 align="center">CTTNHH Chức Đào</h2>
-	</div>
-	<div>
-		<h4 align="center">Địa chỉ: 123 Hải Lộ, Trực Ninh, Nam Định.</h4>
-	</div>
-</div>
--- MVC/view/components/sidebar.php
+++
@@ -1,8 +0,0 @@
-<div class="sidebar">
-	<div id="sidebar_head"><span> </span></div>
-	<div>
-		<span><a href="#">Bánh Trái cây</a></span>
-	</div>
-	<div><span><a href="#">Bánh Socola</a></span></div>
-	<div><span><a href="#">Bánh Ngọt</a></span></div>
-</div>
