ab5441e1bbc45b5a4bf2a9464a79d31c5c767b6d
bigzenoh <bigzenoh@users.noreply.github.com>
2020-12-20 21:59:02 +0700

    login

--- MVC/view/login.php
+++ MVC/view/login.php
@@ -1,25 +1,79 @@
-    
+    <!--
 <a href="http://localhost/WEB/?c=AccountController&m=home" > Trang chủ</a>
 <h2>Login</h2>
-    <form action="?c=AccountController&m=checkLogin" method="POST">
+
+    <div>
+        <form action="?c=AccountController&m=checkLogin" method="POST">
     <div class="form-group">
     <input type="text" name="username" class="form-control"  value="">
     <div class="error">
    
     </div>
     </div>
-    <!-- Close form-group -->
+
     <div class="form-group">
     <input type="text" name="password" class="form-control"  value="">
     <div class="error">
       
     </div>
     </div>
-    <!-- Close form-group -->
+
     <div class="form-group">
     <input type="submit" name="lginBtn" class="btn btn-primary" value="Login">
     <a  href="http://localhost/WEB/?c=AccountController&m=viewSignUpForm" >Sign up</a>
     </div>
-    <!-- Close form-group -->
+
 
     </form>
+    </div>
+    -->
+///////////////////////////////////////////////////////////////////////////
+  <html>
+    <head>
+        <a href="http://localhost/WEB/?c=AccountController&m=home" > Trang chủ</a>
+        <title>
+            Đăng nhập vào website
+        </title>
+        <meta charset="utf-8">
+            <meta content="width=device-width, initial-scale=1.0" name="viewport">
+                <link href="css/fontawesome-free-5.10.2-web/css/all.css" rel="stylesheet" type="text/css"/>
+                <link rel="stylesheet" type="text/css" href="http://localhost/WEB/MVC/public/css/style.css">
+            </meta>
+        </meta>
+        <style>
+            body {
+                background-image: url("http://localhost/WEB/MVC/public/image/wp3980262.jpg");
+            }
+        </style>
+    </head>
+    <body>
+
+        <main id="login-main">
+            <div id="login-container">
+                <div class="login-form">
+                    <form action="?c=AccountController&m=checkLogin" method="POST">
+                        <h1>
+                            Đăng nhập vào website
+                        </h1>
+                        <div class="form-group">
+                            <input type="text" name="username" class="form-control"  value="">
+                            <div class="error"></div>
+                        </div>
+                        <div class="form-group">
+                            <input type="text" name="password" class="form-control"  value="">
+                            <div class="error"></div>
+                        </div>
+                        <div class="form-group">
+                            <a href="http://localhost/WEB/?c=AccountController&m=viewSignUpForm">
+                                <span>
+                                    Chưa có tài khoản? Đăng ký ngay!
+                                </span>
+                            </a>
+                            <input type="submit" name="lginBtn" class="btn btn-primary" value="Đăng nhập">
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </main>
+    </body>
+</html>  
